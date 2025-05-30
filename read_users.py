eff13762672d4bca4e0dd0cd8272e3b641573c10
Rhitam <rhitamnayek17@gmail.com>
2025-05-30 23:43:15 +0530

    Add GitHub Action to print users from JSON-1

--- read_users.py
+++ read_users.py
@@ -1,15 +1,17 @@
 import json
 
-def print_users_from_file(filename):
+def extract_and_save_users(input_file, output_file):
     try:
-        with open(filename, 'r') as file:
+        with open(input_file, 'r') as file:
             data = json.load(file)
             users = data.get("users", [])
-            print("Users:")
-            for user in users:
-                print(user)
+
+        with open(output_file, 'w') as f:
+            json.dump(users, f, indent=2)
+        print(f"'users' data saved to {output_file}")
+
     except Exception as e:
-        print(f"Failed to read or parse {filename}: {e}")
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
-    print_users_from_file("data.json")
+    extract_and_save_users("data.json", "users.json")
